f69fdd91a6bd80ef920296e1591bb0e5fde2c129
cm13 <cmaguluri13@gmail.com>
2022-12-16 19:40:14 +0530

    solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseString = splitString.reverse();
+    let joinReversedString = reverseString.join("");
+    return joinReversedString;
+
 
 };
+console.log(reverseString("hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
